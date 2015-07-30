50ce2c08ee864e3265b2724b08361a97e5f6c2a8
Robert Donovan <admin@mixfin.com>
2015-07-29 21:00:20 -0500

    TTD

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.logger.debug('Starting up Application')
