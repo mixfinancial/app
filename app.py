6889ce654763074026fcfc184e8f84d9b79741b1
Robert Donovan <admin@mixfin.com>
2015-07-29 21:32:49 -0500

    Working on the Layout template

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def Main():
-    return render_template('layout.html')
+def hello_world():
+    return render_template('index.html')
 
-
-@app.route('/about')
+@app.route('/layout')
 def about():
-    return render_template('about.html')
+  return render_template('layout.html')
 
+#@app.route('/about')
+#def about():
+#  return render_template('about.html')
 
 if __name__ == '__main__':
     app.debug = True
